29b124efe7eb1d762839c940df800a27cc33bbc0
matthewlim124 <mattlim124816@gmail.com>
2024-11-12 00:51:06 +0700

    message change

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"Customer_Name": "Matt", "Email": "matt140504@gmail.com", "Order_id": "d10111"}
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
